e56493c4b36bb2f6a36057dae56d3f5868b5bd2b
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-07-12 10:54:09 +0200

    update

--- input.tf
+++ input.tf
@@ -1,1 +1,21 @@
-// region and tags are auto-declared by terragrunt in the generated file tg_vars.tf, so we do NOT declare them here
+//
+// this file will be overwritten by terragrunt
+//
+// used only for unit tests - hence no defaults
+//
+
+variable "region" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "resource_short_name" {
+  type = string
+}
+
+variable "resource_long_name" {
+  type = string
+}
